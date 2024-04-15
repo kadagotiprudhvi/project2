7cb404e111d9e817657e0cac427b2cd74f70547c
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-15 20:19:31 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routes;
 
