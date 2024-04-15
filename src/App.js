e23369f4dd8fbbee3b66b8e39656631c41a52fcc
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-15 20:17:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import './App.css';
+import Menu from './components/Menu';
+import Cart from './components/Cart';
+import SearchBar from './components/SearchBar';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="logo">🐳 FOOD CLOUD</div>
+        <div className="icons">
+          <SearchBar />
+          <Cart />
+          <UserProfile />
+        </div>
+      </header>
+      <main>
+        <Menu />
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
