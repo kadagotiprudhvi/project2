4e568f348e10a132ac92a90ab372604e91bc2c52
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-15 20:12:16 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to Food Cloud!</h2>
+      <p>Discover a variety of delicious dishes from our cloud kitchen.</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
+
+export default Home;
 
