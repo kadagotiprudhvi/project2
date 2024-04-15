c6024682c82abad1d947a2bc82e3b48ae13b9584
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-15 19:53:55 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div className="cart">
+      <img src="cart-icon.png" alt="Cart" />
+      {/* Add cart functionality here */}
+      <h3>Shopping Cart</h3>
+      <ul>
+        {/* Display list of items in the cart */}
+        <li>Item 1</li>
+        <li>Item 2</li>
+        {/* Add more items as needed */}
+      </ul>
+    </div>
+  );
+}
+
+export default Cart;
 
