442d60a1eee7da1050fab754b7ec32b18d1cd299
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-15 19:54:59 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,1 +1,30 @@
+import React from 'react';
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>Menu</h2>
+      <div className="category">
+        <h3>Indian Style</h3>
+        <ul>
+          <li>Butter Chicken</li>
+          <li>Tandoori Tikka</li>
+          <li>Thali</li>
+          {/* Add more Indian style items */}
+        </ul>
+      </div>
+      <div className="category">
+        <h3>Salad</h3>
+        <ul>
+          <li>Greek Salad</li>
+          <li>Caesar Salad</li>
+          {/* Add more salad items */}
+        </ul>
+      </div>
+      {/* Add more categories */}
+    </div>
+  );
+}
+
+export default Menu;
 
