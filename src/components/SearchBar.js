b6bd75436f206a5e1e85087b977e0544b63616e8
kadagotiprudhvi <113254256+kadagotiprudhvi@users.noreply.github.com>
2024-04-15 19:55:43 +0530

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,1 +1,14 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search" />
+      {/* Add search functionality here */}
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
 
